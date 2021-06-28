f60cef2809dc6df432b3b422049eda82aeeadd5e
areinel <alejandro.reinel@bytelondon.com>
2021-06-28 20:19:04 +0100

    Expanded EmployeeFromBob to check Arrays and fixed bug due to typo

--- src/EmployeeType.ts
+++ src/EmployeeType.ts
@@ -1,3 +1,5 @@
+import {Is, isArray, isNumber, isString, isStruct} from "./guards";
+
 export type Employee = {
     tableID: number,
     firstName: string
@@ -8,3 +10,33 @@ export type Employee = {
     bobID: string
     floatID: string
 }
+
+export const isEmployee: Is<Employee> = isStruct({
+    tableID: isNumber,
+    firstName: isString,
+    lastName: isString,
+    startDate: isString,
+    location: isString,
+    email: isString,
+    bobID: isString,
+    floatID: isString,
+})
+
+export const isEmployeeArray: Is<Array<Employee>> = isArray(isEmployee)
+
+
+
+
+export type EmployeeFromBob = Omit<Employee, "tableID" | "floatID">
+
+export const isEmployeeFromBob: Is<Employee> = isStruct({
+    firstName: isString,
+    lastName: isString,
+    startDate: isString,
+    location: isString,
+    email: isString,
+    bobID: isString
+})
+
+export const isEmployeeFromBobArray: Is<Array<EmployeeFromBob>> = isArray(isEmployeeFromBob)
+
