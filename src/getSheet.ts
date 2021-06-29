506dc4093c007fbc6e166ebbdef459288a537907
areinel <alejandro.reinel@bytelondon.com>
2021-06-29 09:58:27 +0100

    Updated Get Sheets to include change logs

---
+++ src/getSheet.ts
@@ -0,0 +1,40 @@
+import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+
+export const getSheet = (sheet: "people" | "changes", create: boolean = false): Sheet => {
+
+    const mainDocument = SpreadsheetApp.getActiveSpreadsheet()
+
+    // Check if sheet exists, if not create it
+    let peopleSheet: Sheet | null = mainDocument.getSheetByName(sheet)
+
+    if (create && !peopleSheet) {
+        mainDocument.insertSheet('People')
+        peopleSheet = mainDocument.getSheetByName(sheet)
+        if (!peopleSheet) {
+            throw new Error('Error Creating Sheet')
+        }
+        const colNames = []
+        switch (sheet) {
+            case "people":
+                colNames.push(
+                    ["TableID", "First Name", "Last Name", "Start Date", "Location", "Email", "Float ID"]
+                )
+                break
+            case "changes":
+                colNames.push(
+                    ["Change Type", "Employee Email", "Request Id", "Policy Type", "Start Date", "Start Portion", "End Date", "End Portion"]
+                )
+                break
+            default:
+                throw new Error("Please input a correct endpoint")
+        }
+
+        peopleSheet.getRange(1, 1, 1, colNames[0].length).setValues(colNames)
+    }
+
+    if (!peopleSheet) {
+        throw new Error('Error Getting Sheet')
+    }
+
+    return peopleSheet
+}
