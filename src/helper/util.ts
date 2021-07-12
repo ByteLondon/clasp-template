ee97c31235265077841e900e15119969c291709f
areinel <alejandro.reinel@bytelondon.com>
2021-07-12 16:52:39 +0100

    Added requestTypeDecoder and implemented testing for all used functions

---
+++ src/helper/util.ts
@@ -0,0 +1,72 @@
+import {memoize} from 'lodash/fp'
+import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+
+
+export const camelize: (s: string) => string = memoize((s: string): string => {
+    return s
+        .replace(/([^A-z0-9 _])/g, ' ')
+        .replace(/^[A-z]/, p => p.toLowerCase())
+        .replace(/[_\s]([a-z0-9])/g, (_, p1) => p1.toUpperCase())
+        .replace(/\s/g, '')
+})
+
+export const toCol = (col: number): string => {
+    let letter = '',
+        temp;
+    while (col > 0) {
+        temp = (col - 1) % 26;
+        letter = String.fromCharCode(temp + 65) + letter;
+        col = (col - temp - 1) / 26;
+    }
+    return letter;
+}
+
+export const getSingleColumn = (dataSheet: Sheet, col: string) => {
+    return dataSheet
+        .getRange(`${col}:${col}`)
+        .getValues()
+        .map(e => e[0])
+        .filter(e => e)
+}
+
+
+export const getFirstEmptyRow = (dataSheet: Sheet): number => {
+    const values = dataSheet.getRange("A:A").getValues()
+    let count = 0
+    while (values[count] && values[count][0] != "") {
+        count+=1
+    }
+    return (count + 1);
+}
+
+export const requestTypeDecoder = (targetType: string): number => {
+    switch (targetType) {
+        case "Birthday Day":
+            return 197160
+        case "Compassionate":
+            return 46000
+        case "Holiday":
+            return 46002
+        case "Maternity / Paternity":
+            return 46000
+        case "Medical Appointment":
+            return 159856
+        case "Personal Appointment (Paid)":
+            return 159856
+        case "Personal Appointment (Unpaid)":
+            return 159856
+        case "Sick":
+            return 46001
+        case "TOIL":
+            return 46000
+        case "Training":
+            return 197161
+        case "Unpaid Leave":
+            return 159206
+        case "WFH due to sickness":
+            return 46001
+        default:
+            throw new Error("Leave Type incorrect/not found")
+    }
+}
+
---
+++ test/helper/util.test.ts
@@ -0,0 +1,54 @@
+import {expect} from 'chai'
+import {getFirstEmptyRow} from "../../src/helper/util";
+import {requestTypeDecoder} from "../../src/helper/util";
+
+
+describe('utils', () => {
+    describe('requestTypeDecoder', () => {
+        it('should return correct Float ID', () => {
+            const func = requestTypeDecoder
+            const floatHolidayType = "Birthday Day"
+            expect(func(floatHolidayType)).to.equal(197160)
+        })
+
+        it('should return Error if not found (Number) ', () => {
+            const func = requestTypeDecoder
+            const floatHolidayType = "not a valid request"
+            expect(() => func(floatHolidayType)).to.throw
+        })
+
+        it('should return Error if not found (Number) ', () => {
+            const func = requestTypeDecoder
+            const floatHolidayType = 2131313 as unknown as string
+            expect(() => func(floatHolidayType)).to.throw
+        })
+    })
+
+
+
+    describe('getFirstEmptyRow', () => {
+        it('Should Return 4 if 3 spaces are taken' ,() => {
+
+            const dataSheet = {
+                getRange: (_: string) => ({
+                    getValues: (): string[][] => [["email1@test.com"],["email2test.com"],["email3@test.com"]]
+                })
+            }
+
+            const func = getFirstEmptyRow
+            expect(func(dataSheet as unknown as GoogleAppsScript.Spreadsheet.Sheet)).to.equal(4)
+        })
+
+        it('Should Return 1 if empty' ,() => {
+
+            const dataSheet = {
+                getRange: (_: string) => ({
+                    getValues: (): string[][] => [[""],[""],[""]]
+                })
+            }
+
+            const func = getFirstEmptyRow
+            expect(func(dataSheet as unknown as GoogleAppsScript.Spreadsheet.Sheet)).to.equal(1)
+        })
+    })
+})
