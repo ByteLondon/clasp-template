5e5db6caafe3a4d61179e5e1e485a6e780ba09bb
areinel <alejandro.reinel@bytelondon.com>
2021-07-12 18:38:16 +0100

    Added getHeader function to Stub

--- test/gasStubs/url-fetch-app.ts
+++ test/gasStubs/url-fetch-app.ts
@@ -1,5 +1,10 @@
-import HTTPResponse = GoogleAppsScript.URL_Fetch.HTTPResponse
-import URLFetchRequest = GoogleAppsScript.URL_Fetch.URLFetchRequest
+import HTTPResponse = GoogleAppsScript.URL_Fetch.HTTPResponse;
+import URLFetchRequest = GoogleAppsScript.URL_Fetch.URLFetchRequest;
+
+type FakeResponse = {
+    content: string
+    headers?: object
+}
 
 /**
  * Stub various responses from specific urls
@@ -8,25 +13,29 @@ import URLFetchRequest = GoogleAppsScript.URL_Fetch.URLFetchRequest
 const responses: Record<string, any> = {}
 
 global.UrlFetchApp = {
-  fetch: (url: string) =>
+    fetch: (url: string) =>
     ({
-      getContentText: () => {
-        return responses[url] ? `${responses[url]}` : '{}'
-      },
+        getContentText: () => {
+            return responses[url]?.content ? `${responses[url].content}` : '{}'
+        },
+        getHeaders: (): object | undefined => {
+            return responses[url]?.headers ? responses[url].headers : {}
+        }
     } as HTTPResponse),
-  fetchAll: () => {
-    return []
-  },
-  getRequest: (url: string) => {
-    console.log(url)
-    return ({ url } as unknown) as URLFetchRequest
-  },
+    fetchAll: () => {
+        return []
+    },
+    getRequest: (url: string) => {
+        console.log(url)
+        return ({url} as unknown) as URLFetchRequest
+    },
 }
 export default {
-  addResponses: (responsesInput: Record<string, any>) => {
-    Object.assign(responses, responsesInput)
-  },
-  removeResponse: (responseKey: string) => {
-    delete responses[responseKey]
-  },
+    addResponses: (responsesInput: Record<string, FakeResponse>) => {
+        Object.assign(responses, responsesInput)
+    },
+    removeResponse: (responseKey: string) => {
+        delete responses[responseKey]
+    },
+
 }
