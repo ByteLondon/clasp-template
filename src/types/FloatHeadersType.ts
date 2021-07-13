9cfa1d4f375b94dc6f5f619a6d61575bd29b321d
areinel <alejandro.reinel@bytelondon.com>
2021-07-13 18:04:30 +0100

    Made some variables optional to reuse type in other requests.

--- src/types/FloatHeadersType.ts
+++ src/types/FloatHeadersType.ts
@@ -1,15 +1,15 @@
-import {Is, isNumber, isStruct} from "../helper/guards";
+import {Is, isNumber, isOptional, isStruct} from "../helper/guards";
 
 export type FloatHeaders = {
-    "X-Pagination-Total-Count": number,
+    "X-Pagination-Total-Count"?: number,
     "X-Pagination-Page-Count": number,
-    "X-Pagination-Current-Page": number,
-    "X-Pagination-Per-Page": number
+    "X-Pagination-Current-Page"?: number,
+    "X-Pagination-Per-Page"?: number
 }
 
 export const isFloatHeaders: Is<FloatHeaders> = isStruct({
-    "X-Pagination-Total-Count": isNumber,
+    "X-Pagination-Total-Count": isOptional(isNumber),
     "X-Pagination-Page-Count": isNumber,
-    "X-Pagination-Current-Page": isNumber,
-    "X-Pagination-Per-Page": isNumber
+    "X-Pagination-Current-Page": isOptional(isNumber),
+    "X-Pagination-Per-Page": isOptional(isNumber)
 })
